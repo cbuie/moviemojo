1870fed1cda302c7dfd88c4fbe5a8be9e93347ec
cbuie <chrisbuie@gmail.com>
2016-09-29 15:22:33 -0700

    update

--- scrape_mov_mojo.py
+++ scrape_mov_mojo.py
@@ -61,7 +61,7 @@ def runtime_to_minutes(runtimestring):
 
 
 # variables year and number of pages to construct url
-#year = range(2011, 2016, 1)
+#year = range(2011, 2016, 1) #marked out to add 2016 data
 year = '2016'
 num_pages = range(1,8)
 
@@ -135,7 +135,7 @@ for l in set(links_list):
 
 import pandas as pd
 movie_data1 = pd.DataFrame(movie_data)
-movie_data1.to_csv('movie_data_2016.csv')
+# movie_data1.to_csv('movie_data_2016.csv')
 
 
 #
